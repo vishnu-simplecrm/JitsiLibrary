1a0da244297dc2835d373cf74c61ce8391209cb1
Your Name <vishnu@simplecrm.com>
2021-03-11 01:40:58 +0530

    added jitsi class

--- app/src/main/java/com/example/jistsiexample/JitsiMainActivity.java
+++ app/src/main/java/com/example/jistsiexample/JitsiMainActivity.java
@@ -37,25 +37,4 @@ public class JitsiMainActivity extends AppCompatActivity {
         });
     }
 
-    public static void startMeeting(Context context, String url, String username, String room_name) throws Exception {
-
-        JitsiMeetUserInfo jitsiMeetUserInfo = new JitsiMeetUserInfo();
-        jitsiMeetUserInfo.setDisplayName(username);
-        jitsiMeetUserInfo.setEmail("vishnu@simplecrm.com");
-
-        JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
-                .setServerURL(new URL(url))
-                .setAudioMuted(false)
-                .setAudioOnly(false)
-                .setRoom(room_name)
-                .setVideoMuted(false)
-                .setUserInfo(jitsiMeetUserInfo)
-                .setFeatureFlag("toolbox.enabled", true)
-                .setFeatureFlag("filmstrip.enabled", true)
-                .setWelcomePageEnabled(false)
-                .build();
-
-        JitsiMeetActivity.launch(context, options);
-        //JitsiMeet.setDefaultConferenceOptions(defaultOptions);
-    }
 }
--- simplecrm_jitsi/src/main/java/com/example/simplecrm_jitsi/JitsiActivity.java
+++ simplecrm_jitsi/src/main/java/com/example/simplecrm_jitsi/JitsiActivity.java
@@ -1,6 +1,35 @@
 package com.example.simplecrm_jitsi;
 
+import android.content.Context;
+
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+import org.jitsi.meet.sdk.JitsiMeetUserInfo;
+
+import java.net.URL;
+
 public class JitsiActivity {
 
+    public static void startMeeting(Context context, String url, String username, String room_name) throws Exception {
+
+        JitsiMeetUserInfo jitsiMeetUserInfo = new JitsiMeetUserInfo();
+        jitsiMeetUserInfo.setDisplayName(username);
+        jitsiMeetUserInfo.setEmail("vishnu@simplecrm.com");
+
+        JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                .setServerURL(new URL(url))
+                .setAudioMuted(false)
+                .setAudioOnly(false)
+                .setRoom(room_name)
+                .setVideoMuted(false)
+                .setUserInfo(jitsiMeetUserInfo)
+                .setFeatureFlag("toolbox.enabled", true)
+                .setFeatureFlag("filmstrip.enabled", true)
+                .setWelcomePageEnabled(false)
+                .build();
+
+        JitsiMeetActivity.launch(context, options);
+        //JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+    }
 
 }
